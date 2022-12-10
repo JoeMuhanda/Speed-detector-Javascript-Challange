0df5e7affd89d69c95e8e97784c08c9eceb6ee2c
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2022-12-10 23:35:12 +0300

    Update Test.js

--- Test.js
+++ Test.js
@@ -3,33 +3,3 @@ const input =readline.createInterface(
 {
 };
 input.question(")
-const  readline = require ('readline')
-const input =readline.createInterface(
-{
-};
-input.question(")
-const  readline = require ('readline')const  readline = require ('readline')
-const input =readline.createInterface(
-{
-};
-input.question(")
-const  readline = require ('readline')
-const input =readline.createInterface(
-{
-};
-input.question(")
-const  readline = require ('readline')
-const input =readline.createInterface(
-{
-};
-input.question(")
-
-const input =readline.createInterface(
-{
-};
-input.question(")
-const  readline = require ('readline')
-const input =readline.createInterface(
-{
-};
-input.question(")
