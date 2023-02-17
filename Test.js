8e22625e8d47d1243402ca8f2a8f0d06198100fd
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-02-17 22:36:52 +0300

    Update Test.js

--- Test.js
+++ Test.js
@@ -5,3 +5,22 @@ const input =readline.createInterface(
 {
 };
 input.question(")
+
+  // Set the speed limit (in miles per hour)
+const speedLimit = 60;
+
+// Define a function to check the speed
+function checkSpeed(speed) {
+  if (speed <= speedLimit) {
+    console.log("You are driving within the speed limit.");
+  } else {
+    const overSpeed = speed - speedLimit;
+    const demeritPoints = Math.floor(overSpeed / 5);
+    console.log(`You are driving over the speed limit by ${overSpeed} mph.`);
+    console.log(`You have received ${demeritPoints} demerit points.`);
+  }
+}
+
+// Test the function with some sample speeds
+checkSpeed(50); // Output: "You are driving within the speed limit."
+checkSpeed(65); // Output: "You are driving over the speed limit by 5 mph." and "You have received 1 demerit points."
